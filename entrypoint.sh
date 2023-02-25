a2e3f02c428846c1b013eb30549fbdf1e4387212
EAminy <57687989+EmilAminy@users.noreply.github.com>
2023-02-25 16:20:57 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir /matrix-docker-ansible-deploy/inventory/host_vars/${MY_DOMAIN}
+mv /vars.yml /matrix-docker-ansible-deploy/inventory/host_vars/${MY_DOMAIN}
+
+sed -i "s/<your_domain>/${MY_DOMAIN}/g" /matrix-docker-ansible-deploy/examples/hosts
+mv /matrix-docker-ansible-deploy/examples/hosts /matrix-docker-ansible-deploy/inventory
+
+docker run -it --rm \
+    -w /work \
+    -v `pwd`:/work \
+    -v $HOME/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
+    --entrypoint=/bin/sh \
+    docker.io/devture/ansible:2.13.6-r0-1
+
+ansible-playbook -i inventory/hosts setup.yml \
+    --extra-vars="username=${ADMIN_USERNAME} password=${ADMIN_PASSWORD} admin=yes" \
+    --tags=register-user
